597383c8ce92bef5a637f7897d6e4d72afd08388
Thiago Santos <thiagossantos@gmail.com>
2015-12-12 10:17:35 -0300

    bidfetcher: use session to get session info
    Use a session to access main site before doing the fetching

--- bidfetcher.py
+++ bidfetcher.py
@@ -1,10 +1,13 @@
 import requests
 from datetime import date
 
+BID_URL = 'http://bid.cbf.com.br'
 BID_REQ_URL = 'http://bid.cbf.com.br/a/bid/carregar/json/'
 
 def bid_get_data_for_date(d):
-   r = requests.post(BID_REQ_URL, data={'uf':'',
+   s = requests.Session()
+   s.get(BID_URL)
+   r = s.post(BID_REQ_URL, data={'uf':'',
                                         'dt_pesquisa':d.strftime('%d/%m/%Y'),
                                         'tp_contrato':'TODOS',
                                         'n_atleta':''})
